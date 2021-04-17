8990e386841679ff1e475eb51233831606157df2
DIEGOFLOREZBELTRAN <diflorez@misena.edu.co>
2021-04-16 21:26:57 -0500

    16-04-2021

---
+++ Ejercicio1.js
@@ -0,0 +1,18 @@
+// 1.	Realizar un programa que permita leer 2 números diferentes y nos diga cual es el mayor de los 2 números.
+
+
+let _indexNumero = 0;
+
+let _arrayNumero = new Array(10);
+
+let menu = "";
+menu += "------------------------------------------------------------\n";
+menu += "--------------------- Menú Principal ---------------------\n"
+menu += "------------------------------------------------------------\n";
+menu += "\n";
+menu += "1. Agregar Numero Par\n";
+menu += "2. Agregar Numero Impar\n";
+menu += "4. Ver ver Numero Par\n";
+menu += "4. Ver ver Numero Impar\n";
+menu += "0. Salir";
+
