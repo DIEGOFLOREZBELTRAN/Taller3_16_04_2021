4ceadaa43db60e4868dd7dcb5c72b3147cc0f924
DIEGOFLOREZBELTRAN <diflorez@misena.edu.co>
2021-04-24 10:08:57 -0500

    24-04-2021

--- Ejercicio1.js
+++ Ejercicio1.js
@@ -1,21 +1,57 @@
 // 1.	Realizar un programa que permita leer 2 números diferentes y nos diga cual es el mayor de los 2 números.
 
 
-let _indexNumero = 0;
+parar:
+do{
+    var num1 = parseInt(prompt("Ingrese el numero 1"));
+    var num2 = parseInt(prompt("Ingrese el numero 2"));
+    if(num1!==num2){
+        console.log(comparacion(num1, num2));
+        break parar;
+    }
+}while(true)
 
-let _arrayNumero = new Array(10);
+function comparacion(num1, num2){
+    if(num1>num2){
+        return `El numero 1: ${num1} es mayor que el numero 2: ${num2}`;
+    }else{
+        return `El numero 2: ${num2} es mayor que el numero 1: ${num1}`;
+    }
+} 
 
-let menu = "";
-menu += "------------------------------------------------------------\n";
-menu += "--------------------- Menú Principal ---------------------\n"
-menu += "------------------------------------------------------------\n";
-menu += "\n";
-menu += "1. Agregar Numero Par\n";
-menu += "2. Agregar Numero Impar\n";
-menu += "4. Ver ver Numero Par\n";
-menu += "4. Ver ver Numero Impar\n";
-menu += "0. Salir";
-let opc;
-		do{
-			opc = parseInt(prompt(menu));
-            let Numero
+
+
+// parar:
+
+// hacer{
+// 	   var num1 = parseInt("Ingrese el numero 1"));
+// 	   var num2 = parseInt("Ingrese el numero 2"));
+// 	   if(num1!==num2)
+// 	     {
+//           consola. log(comparación(num1, num2));
+// 		  romper parar;
+// 	     }
+//      }
+
+
+// let _indexNumero = 0;
+
+// let _arrayNumero = new Array(10);
+
+// let menu = "";
+// menu += "------------------------------------------------------------\n";
+// menu += "--------------------- Menú Principal ---------------------\n"
+// menu += "------------------------------------------------------------\n";
+// menu += "\n";
+// menu += "1. Agregar Numero Par\n";
+// menu += "2. Agregar Numero Impar\n";
+// menu += "4. Ver ver Numero Par\n";
+// menu += "4. Ver ver Numero Impar\n";
+// menu += "0. Salir";
+// let opc;
+// 		do{
+// 			opc = parseInt(prompt(menu));
+//             let Numero
+
+
+
--- Ejercicio5.js
+++ Ejercicio5.js
@@ -1,2 +1,10 @@
 // 5.	Realizar un programa que permita escribir los 100 primeros pares.
 
+
+for (let i = 1; i <= 100; i++) 
+{
+    if(i%2==0)
+      {
+        console.log(`Pares ${i}`);
+      }
+}
